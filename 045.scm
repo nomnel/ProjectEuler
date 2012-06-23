19468df829dd7d2af49af7cee10ea53d0225805c
nomnel <laxyz.nomnel@gmail.com>
2012-06-23 23:55:16 +0900

    solved Problem 45.

---
+++ 045.scm
@@ -0,0 +1,18 @@
+;; Gauche 0.9.3
+
+(add-load-path "." :relative)
+(use eulerlib)
+
+; 六角数をチェックしていくだけ
+(define (e45)
+  (define triangle?
+    (memorize (^y (let1 x (/ (- (sqrt (+ 1 (* 8 y))) 1) 2)
+		    (= x (floor->exact x))))))
+  (define pentagonal?
+    (memorize (^y (let1 x (/ (+ 1 (sqrt (+ 1 (* 24 y)))) 6)
+		    (= x (floor->exact x))))))
+  (define hexagonal (polygonal-formula 6))
+  (let loop ((n 144) (v 41328))
+    (if (and (triangle? v) (pentagonal? v))
+	v
+	(loop (+ n 1) (hexagonal (+ n 1))))))
