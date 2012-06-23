9cb5e94040be0b995baaef624581ca7e510ffd6a
nomnel <laxyz.nomnel@gmail.com>
2012-06-23 23:30:43 +0900

    solved Problem 42.

---
+++ 042.scm
@@ -0,0 +1,19 @@
+;; Gauche 0.9.3
+
+(add-load-path "." :relative)
+(use eulerlib)
+
+; words.txtに含まれる単語の最大長は15なので, 1000(> 15*26)以下の三角数を判定出来れば十分
+(define (e42)
+  (define (word-value w)
+    (apply + (map (^c (- (char->integer c) 64))
+		  (string->list w))))
+  (define triangle?
+    (is? (take-while (cut < <> 1000)
+		     (map (^n ((polygonal-formula 3) n))
+			  (range 1 50)))))
+  (fold (^(w r) (if (triangle? (word-value w))
+		    (+ r 1) r))
+	0
+	(map (^s (substring s 1 (- (string-length s) 1)))
+	     (string-split (call-with-input-file "./words.txt" read-line) ","))))
