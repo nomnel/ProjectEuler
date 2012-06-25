7a1a11a942f795ae56d332012385fce9ae56cad5
nomnel <laxyz.nomnel@gmail.com>
2012-06-26 00:42:43 +0900

    solved Problem 53.

---
+++ 053.scm
@@ -0,0 +1,17 @@
+;; Gauche 0.9.3
+
+(add-load-path "." :relative)
+(use eulerlib)
+
+; nCr = nCn-r だが十分早く計算できるので使わなかった
+(define (e53)
+  (define (combination-count n r)
+    (if (or (zero? r) (zero? n))
+	1
+	(/ (apply * (iota r (+ 1 (- n r))))
+	   (apply * (iota r 1)))))
+  (let loop ((n 23) (r 1) (res 0))
+    (cond ((< 100 n) res)
+	  ((< n r) (loop (+ n 1) 1 res))
+	  (else (loop n (+ r 1) (if (< 1000000 (combination-count n r))
+				    (+ res 1) res))))))
