81dfe3ed04bab0c2003ed5e2dee7a33c87d221c8
nomnel <laxyz.nomnel@gmail.com>
2012-06-26 01:01:09 +0900

    solved Problem 55.

---
+++ 055.scm
@@ -0,0 +1,18 @@
+;; Gauche 0.9.3
+
+(add-load-path "." :relative)
+(use eulerlib)
+
+; 総当たり
+(define (e55)
+  (define (reverse-num n)
+    (list->integer (reverse (integer->list n))))
+  (define (lychrel? n)
+    (let loop ((c 1) (n (+ n (reverse-num n))))
+      (cond ((< 50 c) #t)
+	    ((palindrome? n) #f)
+	    (else (loop (+ c 1) (+ n (reverse-num n)))))))
+  (let loop ((n 10) (res 0))
+    (cond ((< 9999 n) res)
+	  ((lychrel? n) (loop (+ n 1) (+ res 1)))
+	  (else (loop (+ n 1) res)))))
