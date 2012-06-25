8a473a61803318703aac2dff79a480b13d381d8c
nomnel <laxyz.nomnel@gmail.com>
2012-06-26 01:03:38 +0900

    solved Problem 56.

---
+++ 056.scm
@@ -0,0 +1,11 @@
+;; Gauche 0.9.3
+
+(add-load-path "." :relative)
+(use eulerlib)
+
+(define (e56)
+  (apply max
+	 (map (^a (apply max
+			 (map (^b (apply + (integer->list (expt a b))))
+			      (iota 99 1))))
+	      (iota 99 1))))
