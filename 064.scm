4faa0ed4e695db0e9c363ddb4c4299bb0c54d9fb
nomnel <laxyz.nomnel@gmail.com>
2012-06-27 13:57:35 +0900

    rewrite (using continued-fraction).

--- 064.scm
+++ 064.scm
@@ -4,14 +4,11 @@
 (use eulerlib)
 
 (define (e64)
-  (let loop ((n 2) (res 0))
-    (if (< 10000 n) res
-	(let1 lim (floor->exact (sqrt n))
-	  (if (= n (* lim lim))
-	      (loop (+ n 1) res)
-	      (let loop2 ((r lim) (k 1) (period 0))
-		(if (or (not (= k 1)) (zero? period))
-		    (let1 k (/ (- n (* r r)) k)
-		      (loop2 (- (* (quotient (+ lim r) k) k) r)
-			     k (+ period 1)))
-		    (loop (+ n 1) (if (odd? period) (+ res 1) res)))))))))
+  (define (square? n)
+    (= n (expt (floor->exact (sqrt n)) 2)))
+  (let loop ((n 2) (r 0))
+    (if (> n 10000) r
+	(loop (+ n 1)
+	      (if (and (not (square? n))
+		       (odd? (length (cadr (continued-fraction n)))))
+		  (+ r 1) r)))))
