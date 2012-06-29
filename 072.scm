d492dd736cb558e3f8e03196f74daf1bce874fbb
nomnel <laxyz.nomnel@gmail.com>
2012-06-29 22:31:38 +0900

    solved Problem 72.

---
+++ 072.scm
@@ -0,0 +1,14 @@
+;; Gauche 0.9.3
+
+(add-load-path "." :relative)
+(use eulerlib)
+
+; Like a sieve of Eratosthenes.
+(define (e72)
+  (let* ((L (+ 1000000 1))
+	 (phi (list->vector (iota L))))
+    (dolist (i (range 2 L))
+      (when (= i (vector-ref phi i))
+	(dolist (j (range i L i))
+	  (vector-set! phi j (* (vector-ref phi j) (/ (- i 1) i))))))
+    (apply + (vector->list phi))))
