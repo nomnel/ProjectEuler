d760ed6ddf2b530331a5e1c2d66b8040bd66fe3d
nomnel <laxyz.nomnel@gmail.com>
2012-06-29 22:56:08 +0900

    solved Problem 73.

---
+++ 073.scm
@@ -0,0 +1,12 @@
+;; Gauche 0.9.3
+
+(add-load-path "." :relative)
+(use eulerlib)
+
+(define (e73)
+  (let1 res 0
+    (dolist (d (range 5 12001))
+      (dolist (n (range (floor->exact (+ 1 (/ d 3)))
+			(floor->exact (+ 1 (/ (- d 1) 2)))))
+	(when (= 1 (gcd n d)) (inc! res))))
+    res))
