cf69be4cba266be0ce50c20145ea9c55a12b3021
nomnel <laxyz.nomnel@gmail.com>
2012-06-29 19:43:35 +0900

    solved Problem 71.

---
+++ 071.scm
@@ -0,0 +1,9 @@
+;; Gauche 0.9.3
+
+(define (e71)
+  (let loop ((l 2/5) (r 3/7))
+    (let1 M (/ (+ (numerator l) (numerator r))
+	       (+ (denominator l) (denominator r)))
+      (if (< 1000000 (denominator M))
+	  (numerator l)
+	  (loop M r)))))
