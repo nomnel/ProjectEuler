ae6a6843266249beb3bfa0377f3458fe36480dbb
nomnel <laxyz.nomnel@gmail.com>
2012-06-30 13:29:57 +0900

    solved Problem 80.

---
+++ 080.scm
@@ -0,0 +1,11 @@
+;; Gauche 0.9.3
+
+(add-load-path "." :relative)
+(use eulerlib)
+
+(define (e80)
+  (define (irrationals m)
+    (filter (^i (inexact? (sqrt i))) (iota m 1)))
+  ($ apply +
+     $ map (^i (apply + (extract-sqrt i 100)))
+     $ irrationals 100))
