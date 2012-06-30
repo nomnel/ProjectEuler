120b0cc7a6cb7e4c0d1e33b83d10c0f1fa58d9a1
nomnel <laxyz.nomnel@gmail.com>
2012-07-01 01:43:19 +0900

    solved Problem 80 - 83.

---
+++ 081.scm
@@ -0,0 +1,26 @@
+;; Gauche 0.9.3
+
+(add-load-path "." :relative)
+(use eulerlib)
+
+(define (e81)
+  (let* ((len 80)
+	 (V (* len len))
+	 (d (dijkstra))
+	 (cl (apply append
+		    (read-file "./matrix.txt"
+			       (^l (map string->number (string-split l ",")))))))
+    
+    (let loop ((idx 1) (cl cl))
+      (unless (null? cl)
+	; entry point
+	(when (= 1 idx)
+	  ((d 'edge-push!) 0 idx (car cl)))
+	; right
+	(unless (= 1 (modulo idx len))
+	  ((d 'edge-push!) (- idx 1) idx (car cl)))
+	; down
+	(when (< len idx)
+	  ((d 'edge-push!) (- idx len) idx (car cl)))
+	(loop (+ idx 1) (cdr cl))))
+    (floor->exact ((d 'search) V V))))
---
+++ 083.scm
@@ -0,0 +1,32 @@
+;; Gauche 0.9.3
+
+(add-load-path "." :relative)
+(use eulerlib)
+
+(define (e83)
+  (let* ((len 80)
+	 (V (* len len))
+	 (d (dijkstra))
+	 (cl (apply append
+		    (read-file "./matrix.txt"
+			       (^l (map string->number (string-split l ",")))))))
+    
+    (let loop ((idx 1) (cl cl))
+      (unless (null? cl)
+	; entry point
+	(when (= 1 idx)
+	  ((d 'edge-push!) 0 idx (car cl)))
+	; right
+	(unless (= 1 (modulo idx len))
+	  ((d 'edge-push!) (- idx 1) idx (car cl)))
+	; left
+	(unless (zero? (modulo idx len))
+	  ((d 'edge-push!) (+ idx 1) idx (car cl)))
+	; down
+	(when (< len idx)
+	  ((d 'edge-push!) (- idx len) idx (car cl)))
+	; up
+	(when (<= idx (- V len))
+	  ((d 'edge-push!) (+ idx len) idx (car cl)))
+	(loop (+ idx 1) (cdr cl))))
+    (floor->exact ((d 'search) V V))))
