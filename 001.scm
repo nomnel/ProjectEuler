a2f153c31e4d1e5390d4d2d423cc77b4b010f806
nomnel <laxyz.nomnel@gmail.com>
2013-02-13 22:17:32 +0900

    見た目を修正

--- 001.scm
+++ 001.scm
@@ -1,5 +1,5 @@
 ;; Gauche 0.9.3
 (define (e1)
   (apply + (filter (^i (or (zero? (modulo i 3))
-			   (zero? (modulo i 5))))
-		   (iota 1000))))
+            			   (zero? (modulo i 5))))
+        		   (iota 1000))))
