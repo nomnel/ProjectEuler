928c823a2ad7f417be4b944cb6ce9562a0d6b607
nomnel <laxyz.nomnel@gmail.com>
2013-02-20 10:41:17 +0900

    rewrite with lazy sequence

--- 002.scm
+++ 002.scm
@@ -1,7 +1,14 @@
 ;; Gauche 0.9.3
 
-(define (e2)
-  (let loop ((a 1) (b 2) (s 2))
-    (let1 c (+ a b)
-      (if (> c 4000000) s
-	  (loop b c (if (even? c) (+ s c) s))))))
+(use srfi-1)           ; for take-while
+(use gauche.generator) ; for generate
+
+(define (e002b)
+  (define fibs
+    ; (take fibs 10) => (0 1 1 2 3 5 8 13 21 34)
+    (generator->lseq
+      (generate (^(yield) (let loop ((a 0) (b 1))
+                            (yield a) (loop b (+ a b)))))))
+  ($ apply +
+   $ filter (cut even? <>)
+   $ take-while (cut < <> 4000000) fibs))
