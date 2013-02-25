cd26deb2bb05f5728f7c03d20d29a69bd98557e9
nomnel <laxyz.nomnel@gmail.com>
2013-02-25 23:58:32 +0900

    .enum_for(:each_with_index).map -> .map.with_index

--- 022.rb
+++ 022.rb
@@ -4,7 +4,6 @@ def e022
   end
 
   names = File.open("names.txt").read.split(/,/).map{|s| s.delete("\"")}.sort
-  names.enum_for(:each_with_index) \
-       .map{|name, idx| name_score(name) * (idx + 1)} \
+  names.map.with_index{|name, idx| name_score(name) * (idx + 1)} \
        .inject(:+)
 end
