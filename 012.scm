6a12a2781c3d86d3b5b647f0670a5833ad85178d
nomnel <laxyz.nomnel@gmail.com>
2013-08-18 14:30:39 +0900

    rewrite 012.scm

--- 012.scm
+++ 012.scm
@@ -1,11 +1,18 @@
-(add-load-path "." :relative)
-(use eulerlib)
+(use math.prime) ; mc-factorize
+(use gauche.generator) ; generate
 
 (define (e12)
-  (let ((ps (primes 50))
-        (triangle (polygonal-formula 3)))
-    (let loop ((n 29))
-      (let1 c (apply * (map (^l (+ 1 (cadr l))) (factorize (triangle n) ps)))
-        (if (< 500 c)
-            (triangle n)
-            (loop (+ n 1)))))))
+  (define triangles
+    (generator->lseq
+      (generate (^y (let loop ((n 2) (r 1))
+                      (y r) (loop (+ n 1) (+ r n)))))))
+  (define (divisors-count n)
+    (let1 each-factor-counts
+          (cadr (fold (^(n p)
+                         (if (= n (car p))
+                             (list (car p) (cons (+ (caadr p) 1) (cdadr p)))
+                             (list n (cons 1 (cadr p)))))
+                      '(0 ())
+                      (mc-factorize n)))
+      (apply * (map (cut + <> 1) each-factor-counts))))
+  (find (^t (< 500 (divisors-count t))) triangles))
